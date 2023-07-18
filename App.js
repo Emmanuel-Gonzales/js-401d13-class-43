3ea99db1cd150c8869d868d0ac802e58aedf87b3
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-07-17 20:56:01 -0500

    lab 43

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { useState } from 'react';
+import Contact from './Components/Contact';
 import { StyleSheet, Text, View } from 'react-native';
+import { ContainerTypes } from 'expo-contacts';
 
 export default function App() {
+  const [isChecked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.checkedContainer}>
+        <View style={styles.section}>
+          <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} />
+          <Text style={styles.paragraph}>Show Contacts</Text>
+        </View>
+      </View>
+      {
+        isChecked
+          ? <Contact />
+          : <Text style={styles.text} >Welcome to My App</Text>
+      }
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+
+  },
+  text: {
+    margin: 0,
+  },
+  checkedContainer: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 32,
+    // marginTop: 100,
+    margin: 0,
+    padding: 0,
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
   },
 });
---
+++ Components/Contact.js
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, FlatList, Linking, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as Haptics from 'expo-haptics';
+import { useState, useEffect, useCallback } from 'react';
+
+const call = (contact) => {
+  // console.log('from button press', contact)
+  // console.log('contact name', contact.name)
+  let phoneNumber = contact.phoneNumbers[0].number; 
+  // console.log('contact number', phoneNumber)
+  let cleanNumber = phoneNumber.replace(/\D/g, '');
+  const link = `tel:+${cleanNumber}`
+  console.log('link:', link)
+  Linking.canOpenURL(link).then(supported => Linking.openURL(link)).catch(e => console.error(e));
+}
+
+// function stringCheck(item) {
+//   if (typeof (item.name) !== undefined) {
+//     return item.phoneNumbers[0].number.toString();
+//   } else { return item.name }
+// }
+
+const Item = ({ item }) => (
+  <View style={styles.item}>
+    <Button
+      onPress={() => {
+        call(item)
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+      }}
+      title={item.name}
+    />
+    {/* <Text style={styles.title}>{title}</Text> */}
+  </View>
+);
+
+export default function Contact() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const getContacts = async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      console.log('status-------', status)
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync();
+        // console.log('this is my data', data[0].name)
+        setContacts(data)
+      }
+    }
+    getContacts();
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Contacts</Text>
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => <Item item={item} />}
+        keyExtractor={item => item.id}
+      />
+      {/* {
+        contacts.map((contact, index) => (
+          <Text key={`contact-${index}`}>{contact.name}</Text>
+        ))
+      } */}
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 0,
+    paddingTop: 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  }
+});
