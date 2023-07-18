b33f31fd41c4641c2d4ef6912ec38ede04fa4174
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-07-17 20:59:50 -0500

    updated lab

--- Components/Contact.js
+++ Components/Contact.js
@@ -73,7 +73,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 0,
-    paddingTop: 0,
   },
   item: {
     backgroundColor: '#f9c2ff',
